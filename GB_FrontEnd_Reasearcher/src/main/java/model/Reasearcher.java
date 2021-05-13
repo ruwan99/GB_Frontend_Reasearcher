a7e5c48f1181d140165daa6c3f4e0a608390e2d5
ruwan99 <68701495+ruwan99@users.noreply.github.com>
2021-05-13 21:27:46 +0530

    DB Connection implemented

--- GB_FrontEnd_Reasearcher/src/main/java/model/Reasearcher.java
+++ GB_FrontEnd_Reasearcher/src/main/java/model/Reasearcher.java
@@ -1,5 +1,27 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Reasearcher {
+	
+	// A common method to connect to the DB
+		private Connection connect() {
+			Connection con = null;
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				// Provide the correct details: DBServer/DBName, username, password
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/reasearcher", "root", "");
+
+				// For testing
+				System.out.print("Successfully connected");
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return con;
+		}
 
 }
